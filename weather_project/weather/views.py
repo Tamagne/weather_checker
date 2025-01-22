9456437f6bcd8d0085dfdb5cf43a16791d31b6d1
Tamagne <tamagne13@gmail.com>
2025-01-22 16:54:05 +0300

    To deploy Weather Checker Website

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -2,19 +2,20 @@ import requests
 from django.shortcuts import render
 
 def weather_home(request):
+    city = request.GET.get('city', '')
     weather_data = {}
-    city = request.GET.get('city')  # Get the city from the user's input
+
     if city:
-        api_key = 'your_openweathermap_api_key'
-        base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-        response = requests.get(base_url)
+        api_key = 'Yb9d45f04544c7c68e2cc82050454e952'  # Replace with your OpenWeatherMap API Key
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        response = requests.get(url)
         if response.status_code == 200:
             weather_data = response.json()
         else:
-            weather_data = {"error": "City not found"}
-    
+            weather_data['error'] = "City not found. Please try again."
+
     context = {
-        'weather_data': weather_data,
         'city': city,
+        'weather_data': weather_data,
     }
     return render(request, 'weather/weather_home.html', context)
--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from decouple import config
+
+OPENWEATHER_API_KEY = config('OPENWEATHER_API_KEY', default='')  # Add default='' to avoid crashing if not found
